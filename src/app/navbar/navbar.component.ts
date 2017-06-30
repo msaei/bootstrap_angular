685ddcd867365393c3566d96de353ff836127f1a
mahdi saeikhalili <mahdi.saeikhalili@gmail.com>
2017-06-30 12:19:03 -0400

    feat(navbar): bind to component variables

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  public brand: any = { title: "MyCodeDiary", url: "#" };
+  public links: any[] = [
+    { title: 'Home', url: '#'},
+    { title: 'Tutorials', url: '#'},
+    { title: 'Cheat Sheets', url: '#'},
+    { title: 'Tools', url: '#'}
+  ]
 
   constructor() { }
 
