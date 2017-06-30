2b1f5d334bc0930cc95e7a8c697b5a43c0008617
mahdi saeikhalili <mahdi.saeikhalili@gmail.com>
2017-06-30 15:50:41 -0400

    feat(content-right): details variable bind

--- src/app/content-right/content-right.component.ts
+++ src/app/content-right/content-right.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-right.component.css']
 })
 export class ContentRightComponent implements OnInit {
+  public details: any[] = [
+    {
+      title: 'slack',
+      desc: 'slack is a chat app with lots of feature that makes it usefull for teamwork.',
+      url: 'https://slack.com/'
+    },
+    {
+      title: 'slack',
+      desc: 'slack is a chat app with lots of feature that makes it usefull for teamwork.',
+      url: 'https://slack.com/'
+    },
+    {
+      title: 'slack',
+      desc: 'slack is a chat app with lots of feature that makes it usefull for teamwork.',
+      url: 'https://slack.com/'
+    },
+  ]
+  
 
   constructor() { }
 
