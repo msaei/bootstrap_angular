a4c3b1e6eaac3626f35469d47f31d5c11e55f074
mahdi saeikhalili <mahdi.saeikhalili@gmail.com>
2017-06-30 12:26:06 -0400

    feat(header): bind to component variables

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  public title: string = "My Code Diary";
+  public desc: string=`I put everything I learned during my apprenticeship at Checkmate digital everyday here,
+   including usefull links , softwares and tools and tutorial resources.`;
+ 
   constructor() { }
 
   ngOnInit() {
